d2cb732b2398d2644d901f8c5b5dffd271345d1d
GwangJin Byeon <qusrhkdwls45@gmail.com>
2023-10-19 20:15:33 +0900

    미션4 api를 분리하고 기존 코드 개선

--- mission4/api.js
+++ mission4/api.js
@@ -1,33 +1,36 @@
-async function request(url, options){
+const END_POINT = 'https://todo-api.roto.codes';
+
+export async function request(url, options){
     // 이렇게 fetch 같은걸 한 번 래핑해 놓으면 공통적인 변경 사항에 대한 수정이 쉽다.
     // 예를 들어 axios로 변경 한다면 그냥 fetch 부분만 변경해주면 전부 적용이 되는 것이라 볼 수 있음.
+    // 모든 걸 래핑하려는 것은 소모적인 일일 수 있지만 api 호출 처럼 몇가지 유용한 상황이 분명히 있음.
     const res = await fetch(url, options);
     return await res.json();
 }
 
-async function fetchData(username) {
-    return request(`https://todo-api.roto.codes/${username}`);
+export async function fetchData(username) {
+    return request(`${END_POINT}/${username}`);
 }
 
-async function toggle(username, id){
-    return request(`https://todo-api.roto.codes/${username}/${id}/toggle`, { 
+export async function toggle(username, id){
+    return request(`${END_POINT}/${username}/${id}/toggle`, { 
         method: 'PUT' 
     });
 }
 
-async function remove(username, id){
-    return request(`https://todo-api.roto.codes/${username}/${id}`, {
+export async function removeTodo(username, id){
+    return request(`${END_POINT}/${username}/${id}`, {
         method: 'DELETE',
     });
 }
 
-async function createTodo(username, todoText){
-    await fetch(`https://todo-api.roto.codes/${username}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
+export async function createTodo(username, todoText){
+    await fetch(`${END_POINT}/${username}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
             content: todoText,
         }),
     })
--- mission4/main.js
+++ mission4/main.js
@@ -1,57 +1,43 @@
+import { fetchData, toggle, removeTodo, createTodo } from './api.js';
+import TodoList from './TodoList.js';
+
 (async function () {
     const $target = document.querySelector('.App')
   
     const username = 'roto'
   
-    async function fetchData() {
-      const res = await fetch(`https://todo-api.roto.codes/${username}`)
-      return await res.json()
-    }
-  
-    const data = await fetchData()
+    const data = await fetchData(username)
     console.log('data',data);
   
     const todoList = new TodoList({
       $target,
       initialState: data,
       onClick: async function (id) {
-        await fetch(`https://todo-api.roto.codes/${username}/${id}/toggle`, {
-          method: 'PUT',
-        })
+        await toggle(username, id);
   
         // 데이터 추가 후 서버에서 목록 다시 불러서 다시 그리기
-        const updatedData = await fetchData()
+        const updatedData = await fetchData(username)
         todoList.setState(updatedData)
       },
       onRemove: async function (id) {
-        await fetch(`https://todo-api.roto.codes/${username}/${id}`, {
-          method: 'DELETE',
-        })
+        await removeTodo(username, id);
   
         // 데이터 추가 후 서버에서 목록 다시 불러서 다시 그리기
-        const updatedData = await fetchData()
+        const updatedData = await fetchData(username)
         todoList.setState(updatedData)
       },
     })
   
-    document.querySelector('#add-todo-button').addEventListener('click', async function () {
-      const todoText = document.querySelector('#todo-input').value
+    // document.querySelector('#add-todo-button').addEventListener('click', async function () {
+    //   const todoText = document.querySelector('#todo-input').value
   
-      if (todoText.length > 0) {
-        // 데이터 추가하기
-        await fetch(`https://todo-api.roto.codes/${username}`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            content: todoText,
-          }),
-        })
+    //   if (todoText.length > 0) {
+    //     // 데이터 추가하기
+    //     await createTodo(username, todoText);
   
-        // 데이터 추가 후 서버에서 목록 다시 불러서 다시 그리기
-        const updatedData = await fetchData()
-        todoList.setState(updatedData)
-      }
-    })
+    //     // 데이터 추가 후 서버에서 목록 다시 불러서 다시 그리기
+    //     const updatedData = await fetchData()
+    //     todoList.setState(updatedData)
+    //   }
+    // })
   })()
